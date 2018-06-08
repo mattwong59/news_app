4c9d78547004f41dab031e862e901b2733a9e79c
Matt Wong <mattwong59@gmail.com>
2018-06-08 14:26:10 -0700

    Initial commit with news api functionality

---
+++ src/news.js
@@ -0,0 +1,9 @@
+const url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=f1a83c9b40c7495295b01c575bd1ef68";
+
+export async function getNews() {
+    let result = await fetch(url)
+                .then(response => response.json());
+    return result.articles;
+}
+
+
