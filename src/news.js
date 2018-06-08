6b538d905cd37f5b12250386bccb997dacd9600f
Matt Wong <mattwong59@gmail.com>
2018-06-08 15:02:26 -0700

    Change news source to bleacher report

--- src/news.js
+++ src/news.js
@@ -1,4 +1,4 @@
-const url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=f1a83c9b40c7495295b01c575bd1ef68";
+const url = "https://newsapi.org/v2/top-headlines?sources=bleacher-report&apiKey=f1a83c9b40c7495295b01c575bd1ef68";
 
 export async function getNews() {
     let result = await fetch(url)
